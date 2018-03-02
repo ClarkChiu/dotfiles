813a104a7e7b0c8824cc46947c8cff08e8748453
Clark Chiu <clark.chiu.tw@gmail.com>
2018-03-02 10:11:59 +0800

    Temporary commit for update the script

--- script/setup.sh
+++ script/setup.sh
@@ -1,8 +1,10 @@
+# Install the latest Vim from source and setting up the vimrc
+
 # Clone the dotfiles from GitHub
 git clone https://github.com/ClarkChiu/dotfiles ~/dotfiles
 
 # Make the latest vim from source
-#
+sh ./VimCompileScript.sh
 
 # Setup the soft link for vim resource
 mkdir ~/dotfiles/.vim/backup
