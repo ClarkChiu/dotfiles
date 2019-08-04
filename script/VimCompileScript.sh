739f6ca3b72090bc693e08fea63a467c28c14fbb
ClarkChiu <freebooters@gmail.com>
2019-08-04 23:59:22 +0800

    WIP in vim comile and setup

--- script/VimCompileScript.sh
+++ script/VimCompileScript.sh
@@ -1,19 +1,17 @@
 sudo apt-get remove -y --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
 
+sudo apt-get update
 sudo apt-get install -y \
    	python-dev \
    	python3-dev \
    	ruby-dev \
-   	liblua5.2-dev \
    	libperl-dev \
    	libatk1.0-dev \
    	libncurses5-dev \
    	libx11-dev \
    	libxpm-dev \
-   	libxt-dev
-
-#Optional: so vim can be uninstalled again via `dpkg -r vim`
-# sudo apt-get install checkinstall
+   	libxt-dev \
+   	git
 
 sudo rm -rf /usr/local/share/vim /usr/bin/vim
 
@@ -28,25 +26,25 @@ cd src
 make distclean
 cd ..
 
+PYTHON2_CONFIG_DIR='/usr/lib/python2.7/config-x86_64-linux-gnu'
+PYTHON3_CONFIG_DIR='/usr/local/lib/python3.6/config-3.6m-x86_64-linux-gnu'
+
 ./configure \
---enable-multibyte \
---enable-perlinterp=dynamic \
---enable-rubyinterp=dynamic \
---enable-pythoninterp \
---with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
---enable-python3interp \
---with-python3-config-dir=/usr/local/lib/python3.6/config-3.6m-x86_64-linux-gnu \
---enable-luainterp \
---with-luajit \
---enable-cscope \
---enable-gui=auto \
---with-features=huge \
---with-x \
---enable-fontset \
---enable-largefile \
---disable-netbeans \
---with-compiledby="ClarkChiu" \
---enable-fail-if-missing
+	--with-features=huge \
+    --enable-multibyte \
+	--enable-largefile \
+	--enable-rubyinterp=yes \
+	--enable-perlinterp=yes \
+	--enable-luainterp=yes \
+	--enable-pythoninterp=yes \
+	--with-python-config-dir=$PYTHON2_CONFIG_DIR \
+	--enable-python3interp=yes \
+	--with-python3-config-dir=$PYTHON3_CONFIG_DIR \
+	--prefix=/usr/local \
+    --enable-gui=no \
+    --enable-cscope \
+	--disable-netbeans \
+	--with-compiledby="ClarkChiu"
 
 make -j 4 && sudo make install
 
--- script/setup.sh
+++ script/setup.sh
@@ -4,16 +4,17 @@
 # git clone https://github.com/ClarkChiu/dotfiles ~/dotfiles
 
 ## Update the latest Vim plugin manager
+mkdir -p ~/dotfiles/.vim/autoload/
 curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > ~/dotfiles/.vim/autoload/plug.vim
 
 ## Make the latest vim from source
 sh ~/dotfiles/script/VimCompileScript.sh
 
 ## Setup the soft link for vim resource
-mkdir ~/dotfiles/.vim/backup
+mkdir -p ~/dotfiles/.vim/backup
 ln -sf ~/dotfiles/.vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 
 ## Clean and Install the vim plugin
-mkdir ~/dotfiles/.vim/plugged
+mkdir -p ~/dotfiles/.vim/plugged
 vim +'PlugInstall --sync' +qall &> /dev/null
